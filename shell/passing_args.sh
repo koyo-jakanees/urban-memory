e944baa7f19b74f222b328e45f60581f4bd427f3
koyo-jakanees <dakoyo094@gmail.com>
2021-04-16 12:32:51 +0300

    args script

--- shell/passing_args.sh
+++ shell/passing_args.sh
@@ -6,3 +6,18 @@ for FILE1 in "$@"
 do
 	wc $FILE1
 done
+
+echo $0
+# Script with flag args using getopts
+
+# :while getopts u:d:p:f: option
+# do
+# case "${option}"
+# in
+# u) USER=${OPTARG};;
+# d) DATE=${OPTARG};;
+# p) PRODUCT=${OPTARG};;
+# f) FORMAT=${OPTARG};;
+# esac
+# done
+# NVIDIA Tao NGC Mozilla Common voice Nvidia merlin, maxine
