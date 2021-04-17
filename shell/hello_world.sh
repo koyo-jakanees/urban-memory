566696a552d40733c7c8935e7c4af28385c8b736
koyo-jakanees <dakoyo094@gmail.com>
2021-04-17 15:42:37 +0300

    minorr

--- shell/hello_world.sh
+++ shell/hello_world.sh
@@ -405,4 +405,13 @@ ENGLISH_CALC 3 plus 5
 ENGLISH_CALC 5 minus 1
 ENGLISH_CALC 4 times 6
 
-# Special functions
+# Special variables
+# $0 - The filename of the current script.|
+# $n - The Nth argument passed to script was invoked or function was called.|
+# $# - The number of argument passed to script or function.|
+# $@ - All arguments passed to script or function.|
+# $* - All arguments passed to script or function.|
+# $? - The exit status of the last command executed.|
+# $$ - The process ID of the current shell. For shell scripts, this is the process ID under which they are executing.|
+# $! - The process number of the last background command.|
+
