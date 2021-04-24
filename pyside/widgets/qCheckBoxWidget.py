9446b65b12aa395b2580b16eab183867b90341ee
koyo-jakanees <dakoyo094@gmail.com>
2021-04-24 11:02:08 +0300

    checkboxes

--- pyside/widgets/qCheckBoxWidget.py
+++ pyside/widgets/qCheckBoxWidget.py
@@ -1,33 +1,129 @@
 #! /usr/bin/env python3
 #  -*- coding: utf-8 -*-
 
-impor sys
-from PyQt5 import (
-    QApplication,
-    QWidget,
-    QCheckBox
-    QLavbel
-)
+# Beginning PyQt book
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QCheckBox, QLabel
 from PyQt5.QtCore import Qt
 
-class CheckBoxWindow(Qwidget):
-    def __init__():
+
+class CheckBoxWindow(QWidget):
+    """Main widget class."""
+    def __init__(self):
         super().__init__()
-        self.initializUI(
-     
-     def initialize(self)
-        """Initialize the wndow and display irs content to the  to the screen
+        self.initializeUI()
+
+    def initializeUI(self):
+        """Initialize the wndow and display irs content to the  to the screen.
         """
-        self.setGeometry(100,100,250,259
-        self.setWindowTile('QCheckBox Widget')
+        self.setGeometry(100, 100, 250, 250)
+        self.setWindowTitle("QCheckBox Widget")
         self.displayCheckBoxes()
         self.show()
-    
+
+    def displayCheckBoxes(self):
+        """sets up the checkboxs and other widgets."""
+        header_label = QLabel(self)
+        header_label.setText("which shifts can you work? (Please check all that apply)")
+        header_label.setWordWrap(True)
+        header_label.move(10, 10)
+        header_label.resize(230, 60)
+
+        # check boxes setup
+        morning_cb = QCheckBox("Morning [8AM - 2PM]", self)  # text, parent
+        morning_cb.move(20, 80)
+        morning_cb.toggle()  # commnent if you don't want box to start off checked
+        morning_cb.stateChanged.connect(self.printToTerminal)
+
+        after_cb = QCheckBox("Afternoon [1PM - 3AM]", self)  # text, parent
+        after_cb.move(20, 100)
+        after_cb.stateChanged.connect(self.printToTerminal)
+
+        night_cb = QCheckBox("Night [7PM - 3AM]", self)  # text, parent
+        night_cb.move(20, 120)
+        night_cb.stateChanged.connect(self.printToTerminal)
+
+    def printToTerminal(self, state):
+        """ Simple function to show how to determine the state of a checkbox
+        Prints the text label of the checkbox by determining which widget is
+        sending signal.
+
+        Args:
+            state [bool]: signal status
+        """
+        sender = self.sender()
+        if state == Qt.Checked:
+            print(f"{sender.text()} Selected")
+        else:
+            print(f"{sender.text()} Deselected")
+
+
+# Run the program
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CheckBoxWindow()
+    sys.exit(app.exec_())
+'''
+# original script
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QCheckBox, QLabel
+from PyQt5.QtCore import Qt
+
+
+class CheckBoxWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        """
+        Initialize the window and display its contents to the screen.
+        """
+        self.setGeometry(100, 100, 250, 250)
+        self.setWindowTitle("QCheckBox Widget")
+        self.displayCheckBoxes()
+        self.show()
+
     def displayCheckBoxes(self):
         """
-        setup the checkboxs and other widgets
+        Setup the checkboxes and other widgets 
         """
-        
-        header_label = QLable(self)
-        header_lable.setText("which shits can you work")
+        header_label = QLabel(self)
+        header_label.setText("Which shifts can you work? (Please check all that apply)")
         header_label.setWordWrap(True)
+        header_label.move(10, 10)
+        header_label.resize(230, 60)
+        # Set up checkboxes
+        morning_cb = QCheckBox("Morning [8 AM-2 PM]", self)  # text, parent
+        morning_cb.move(20, 80)
+        # morning_cb.toggle() # uncomment if you want box to start off checked,
+        # shown as an example here.
+        morning_cb.stateChanged.connect(self.printToTerminal)
+        after_cb = QCheckBox("Afternoon [1 PM-8 PM]", self)  # text, parent
+        after_cb.move(20, 100)
+        after_cb.stateChanged.connect(self.printToTerminal)
+
+        night_cb = QCheckBox("Night [7 PM-3 AM]", self)  # text, parent
+        night_cb.move(20, 120)
+        night_cb.stateChanged.connect(self.printToTerminal)
+
+    def printToTerminal(self, state):  # pass state of checkbox
+        """
+        Simple function to show how to determine the state of a checkbox.
+                
+        Prints the text label of the checkbox by determining which widget
+        is sending the signal.
+        """
+        sender = self.sender()
+        if state == Qt.Checked:
+            print("{} Selected.".format(sender.text()))
+        else:
+            print("{} Deselected.".format(sender.text()))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CheckBoxWindow()
+    sys.exit(app.exec_())
+'''
