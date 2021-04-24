4e3083ab51069da489970569dc0133aaf7986339
koyo-jakanees <dakoyo094@gmail.com>
2021-04-24 19:49:14 +0300

    side6 not so diff at all

--- pyside/widgets/emptyWindow6.py
+++ pyside/widgets/emptyWindow6.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
-    """
-    Get hands dirty with PySide6
-    """
+"""
+Get hands dirty with PySide6
+"""
+import sys
+from PySide6.QtWidgets import QApplication, QWidget
+
+class EmptyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+        
+    def setupUi(self):
+        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle('PySide6 swimming through')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = EmptyWindow()
+    sys.exit(app.exec_())
