21d99430459e581f87ca2ede2430716aed5af461
koyo-jakanees <dakoyo094@gmail.com>
2021-04-24 20:48:06 +0300

    pixmap on qlabel

--- pyside/widgets/emptyWindow6.py
+++ pyside/widgets/emptyWindow6.py
@@ -2,9 +2,11 @@
 # -*- coding:utf-8 -*-
 """
 Get hands dirty with PySide6
+random online so=nippets, not my work to license
 """
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QLabel, QWidget
+from PySide6.QtGui import QPixmap
 
 class EmptyWindow(QWidget):
     def __init__(self):
@@ -14,7 +16,27 @@ class EmptyWindow(QWidget):
     def setupUi(self):
         self.setGeometry(100, 100, 400, 300)
         self.setWindowTitle('PySide6 swimming through')
+        self.displayLabels()
         self.show()
+        
+    def displayLabels(self):
+        """Display text and images using qlabels
+        catch err if file doesn't exist
+        """
+        txt = QLabel(self)
+        txt.setText('Hello Yoda')
+        txt.move(105, 15)
+        
+        img = '../../img/snippet.png'
+        try:
+            with open(img):
+                code_img = QLabel(self)
+                pixmap = QPixmap(img)
+                code_img.setPixmap(pixmap)
+                code_img.move(25, 40)
+        except FileNotFoundError:
+            print("Image file not found!!")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
