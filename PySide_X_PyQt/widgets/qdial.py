009df9785d1bf7ef2b59b308e808a7f15746d108
koyo-jakanees <dakoyo094@gmail.com>
2021-04-30 15:03:41 +0300

    qdial

---
+++ PySide_X_PyQt/widgets/qdial.py
@@ -0,0 +1,42 @@
+# -*-coding:utf-8 -*-
+# ref: https://pythonbasics.org/qdial/
+
+import sys
+from PyQt5.QtWidgets import (
+    QGridLayout,
+    QDial,
+    QWidget,
+    QApplication,
+    QLabel
+)
+
+class QdialWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi()
+
+    def setupUi(self):
+        layout = QGridLayout()
+        self.setLayout(layout)
+        self.dial = QDial()
+        self.dial.setMinimum(0)
+        self.dial.setMaximum(100)
+        self.dial. setValue(0)
+        self.dial.valueChanged.connect(self.dialerMoved)
+        self.label = QLabel("0")
+        
+        layout.addWidget(self.dial)
+        layout.addWidget(self.label)
+        self.show()
+    
+    def dialerMoved(self):
+        print(f'Dial Value is : {self.dial.value()}')
+        self.label.setText(f'Dial Value is : {self.dial.value()}')
+
+def main():
+    app = QApplication(sys.argv)
+    window = QdialWindow()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
