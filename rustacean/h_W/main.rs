5836b154b95b0320c9e959f1efd441d41ed077c9
koyo-jakanees <dakoyo094@gmail.com>
2021-05-03 11:28:49 +0300

    rust shadowing

--- rustacean/h_W/main.rs
+++ rustacean/h_W/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, World!!")
+    let number = 5; // the first binding created using the name 'number'
+    let number = number + 15; //different binding shadows the name 'number
+    let number = number * 2; //a new binding is created
+    println!("the number is : {}", number);
+    println!("Hello, World!!");
 }
--- rustacean/h_W/var.rs
+++ rustacean/h_W/var.rs
@@ -1,7 +1,11 @@
 fn main() {
-  let a_number = 10;
+  let mut a_number = 10;
   let a_bool = true;
 
   println!("the number is {}", a_number);
   println!("the boolean is {}", a_bool);
+  a_number = 15;
+  println!("And now the number is {}", a_number);
 }
+// variable bindings are immutable by default
+// mut keyword allows us to reassign the variable
