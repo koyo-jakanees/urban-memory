f9751f2172d13f1d855e8889ad0601fd37677f5b
koyo-jakanees <dakoyo094@gmail.com>
2021-05-06 13:14:09 +0300

    save textedit values to file

--- PySide_X_PyQt/widgets/dockingWindow.py
+++ PySide_X_PyQt/widgets/dockingWindow.py
@@ -14,7 +14,8 @@ from PyQt5.QtWidgets import (
     QApplication,
     QAction,
     qApp,
-    QStyle
+    QStyle,
+    QLabel
 )
 from PyQt5.QtCore import Qt
 
@@ -25,11 +26,22 @@ class DockingWindow(QMainWindow):
 
     def setupUi(self):
         self.setWindowTitle('Docky Docky')
-        layout = QHBoxLayout()
-        
+        self.layout = QHBoxLayout()
+
+        self.label = QLabel("The toggle state is ")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.label)
+
+
+        # Add toggle action for checkable menu items
+        toggleAction = QAction('&Toggle Label', self, checkable=True)        
+        toggleAction.setStatusTip('Toggle the label')
+        toggleAction.triggered.connect(self.toggleLabel)
         # Add action for quiting application
         exitAction = QAction(
             self.style().standardIcon(QStyle.SP_DialogCancelButton), 'Q&uit', self)
+        # to use custom image icon, load using QIcon from QtGui as shown.
+        # exitAction = QAction(QIcon('exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl + Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
@@ -41,6 +53,7 @@ class DockingWindow(QMainWindow):
         fileMenu = menuBar.addMenu('&File')
         fileMenu.addAction('New')
         fileMenu.addAction('S&ave')
+        fileMenu.addAction(toggleAction)
         fileMenu.addAction(exitAction)
 
         self.items = QDockWidget('Dockers', self)
@@ -54,18 +67,31 @@ class DockingWindow(QMainWindow):
         self.listwidget.addItem('Item6')
         self.listwidget.addItem('Item7')
 
+        # textEdit properties
+        self.textEdit_value = QTextEdit()
+        self.textEdit_value.textChanged.connect(self.save_text)
+
         self.items.setWidget(self.listwidget)
         self.items.setFloating(False)
-        self.setCentralWidget(QTextEdit())
+        self.setCentralWidget(self.textEdit_value)
         self.addDockWidget(Qt.RightDockWidgetArea, self.items)
 
-        self.setLayout(layout)
+        # self.setLayout(layout)
+        # commented out setting layout to surpress the warning: 
+        # QWidget::setLayout: Attempting to set QLayout "" on DockingWindow "", which already has a layout
 
+    def toggleLabel(self, state):
+        self.label.setText("The toggle state is {}".format(state))
+
+    def save_text(self):
+        text = self.textEdit_value.toPlainText()
+        with open('test.txt', 'w') as f:
+            f.write(text)
 def main():
     app = QApplication(sys.argv)
     dock = DockingWindow()
     dock.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
