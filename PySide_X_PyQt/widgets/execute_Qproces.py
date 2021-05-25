0e07ee140929c3560372dda69d147e2dea7d8f70
koyo-jakanees <dakoyo094@gmail.com>
2021-05-25 12:37:24 +0300

    tutorial qprocess

--- PySide_X_PyQt/widgets/execute_Qproces.py
+++ PySide_X_PyQt/widgets/execute_Qproces.py
@@ -3,10 +3,12 @@
 
 # source blog: https://www.mfitzp.com/tutorials/qprocess-external-programs/
 # running background programs without impacting ui.
+import sys
+import re
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QPlainTextEdit,
                                 QVBoxLayout, QWidget)
 from PyQt5.QtCore import QProcess
-import sys
+
 
 
 class MainWindow(QMainWindow):
@@ -19,6 +21,8 @@ class MainWindow(QMainWindow):
         self.text = QPlainTextEdit()
         self.text.setReadOnly(True)
 
+        self.p = None
+
         l = QVBoxLayout()
         l.addWidget(self.btn)
         l.addWidget(self.text)
@@ -29,9 +33,53 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(w)
 
     def start_process(self):
-        # We'll run our process here.
-        pass
+        if self.p is None:
+            self.message("Executing process.")
+            self.p = QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
+            self.p.readyReadStandardOutput.connect(self.handle_stdout)
+            self.p.readyReadStandardError.connect(self.handle_stderr)
+            self.p.stateChanged.connect(self.handle_state)
+            # self.p.finished.connect(self.cleanup)
+            self.p.finished.connect(self.process_finished)
+            self.p.start("python3", ['external.py'])
+    
+    def message(self, s):
+        self.text.appendPlainText(s)
 
+    def process_finished(self):
+        self.message('Process finished')
+        self.p = None
+
+    def handle_stderr(self):
+        data = self.p.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        self.message(stderr)
+    
+    def handle_stdout(self):
+        data = self.p.readAllStandardOutput()
+        stdout = bytes(data).decode("utf8")
+        self.message(stdout)
+
+    def handle_state(self, state):
+        states = {
+            QProcess.NotRunning: 'Not running',
+            QProcess.Starting: 'Starting',
+            QProcess.Running: 'Running',
+        }
+        state_name = states[state]
+        self.message(f"State changed: {state_name}")
+
+progress_re = re.compile("Total complete: (\d+)%")
+
+def simple_percent_parser(output):
+    """
+    Matches lines using the progress_re regex,
+    returning a single integer for the % progress.
+    """
+    m = progress_re.search(output)
+    if m:
+        pc_complete = m.group(1)
+        return int(pc_complete)
 app = QApplication(sys.argv)
 
 w = MainWindow()
