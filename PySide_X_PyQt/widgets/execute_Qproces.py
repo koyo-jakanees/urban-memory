6e8ff31360a2efee689da98c25aef3815ffe58c6
koyo-jakanees <dakoyo094@gmail.com>
2021-05-25 12:16:28 +0300

    application skeleton layout

--- PySide_X_PyQt/widgets/execute_Qproces.py
+++ PySide_X_PyQt/widgets/execute_Qproces.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
 
-# TLDR: https://www.mfitzp.com/tutorials/qprocess-external-programs/
+# source blog: https://www.mfitzp.com/tutorials/qprocess-external-programs/
 # running background programs without impacting ui.
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QPlainTextEdit,
+                                QVBoxLayout, QWidget)
+from PyQt5.QtCore import QProcess
+import sys
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.btn = QPushButton("Execute")
+        self.btn.pressed.connect(self.start_process)
+        self.text = QPlainTextEdit()
+        self.text.setReadOnly(True)
+
+        l = QVBoxLayout()
+        l.addWidget(self.btn)
+        l.addWidget(self.text)
+
+        w = QWidget()
+        w.setLayout(l)
+
+        self.setCentralWidget(w)
+
+    def start_process(self):
+        # We'll run our process here.
+        pass
+
+app = QApplication(sys.argv)
+
+w = MainWindow()
+w.show()
+
+app.exec_()
