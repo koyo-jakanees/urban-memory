c9b7d02a625d6283c9e1df8cf10243d6122b7982
koyo-jakanees <dakoyo094@gmail.com>
2021-05-27 17:14:16 +0300

    error handling csv tutor

---
+++ rustacean/csvtutor/src/tutorial-error1.rs
@@ -0,0 +1,28 @@
+// official rust csv crate tutorial
+// https://docs.rs/csv/1.1.6/csv/tutorial/index.html
+// the cookbook: https://docs.rs/csv/1.1.6/csv/cookbook/index.html
+
+// import standard library's I/O module to read from stdin
+use std::io;
+use std::process;
+// the 'main' function where the program starts
+fn main() {
+    println!("Hello, world!");
+    let mut reader = csv::Reader::from_reader(io::stdin());
+
+    // loop over each record in the file
+    for result in reader.records() {
+        // examine the result
+        // if there's no problem, print the record otherwise print the error messeg
+        // let record = result.expect("a csv record");
+        // print a debug version of the record
+        // println!("{:?}", record)
+        match result {
+            Ok(record) => println!("{:?}", record),
+            Err(err) => {
+                println!("error reading CSV from <stdin>: {}", err);
+                process::exit(1);
+            }
+        }
+    }
+}
